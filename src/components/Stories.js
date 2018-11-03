cd064fcba3866a149ed290ab3cec930e519b61c0
sfyr111 <sfyr111@163.com>
2018-11-03 14:15:20 +0800

    le

---
+++ src/components/Stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export function Stories (props) {
+  return (
+    <pre>
+      <code>
+        {JSON.stringify(props, null, 2)}
+      </code>
+    </pre>
+  )
+}
+
+function mapState(state) {
+  return state;
+}
+
+export default connect(mapState)(Stories)
