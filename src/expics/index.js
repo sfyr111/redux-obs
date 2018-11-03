f6ecb547eedb6a427ac7bb4b733a35d676624fb9
sfyr111 <sfyr111@163.com>
2018-11-03 21:29:18 +0800

    l5

--- src/expics/index.js
+++ src/expics/index.js
@@ -1,15 +1,15 @@
-import { of } from 'rxjs'
+import { ajax } from 'rxjs/ajax'
 import { combineEpics, ofType } from 'redux-observable'
-import { clear, LOAD_STORIES } from '../actions/index'
-import { switchMap, delay } from 'rxjs/operators'
+import { FETCH_USER, fetchUserFulfilledAction } from "../actions/index";
+import { switchMap, map } from 'rxjs/operators'
 
 function loadStoriesEpic(action$) {
-  console.log(action$)
   return action$.pipe(
-    ofType(LOAD_STORIES),
-    switchMap(() => of(clear()).pipe(delay(2000)))
+    ofType(FETCH_USER),
+    switchMap(({ payload }) => ajax.getJSON(`https://api.github.com/users/${payload}`).pipe(
+      map(user => fetchUserFulfilledAction(user))
+    ))
   )
-
 }
 
 export const rootEpic = combineEpics(loadStoriesEpic)
