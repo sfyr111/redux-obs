7d593b61e9cd9792904fa70e9846edf26216d768
sfyr111 <sfyr111@163.com>
2018-11-03 22:43:41 +0800

    l8

--- src/expics/index.js
+++ src/expics/index.js
@@ -1,4 +1,4 @@
-import { forkJoin, of } from 'rxjs'
+import { forkJoin, of, throwError } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
 import { switchMap, map, mergeMap, debounceTime, catchError } from 'rxjs/operators'
 import { combineEpics, ofType } from 'redux-observable'
@@ -6,7 +6,10 @@ import { receiveBeers, searchBeersError, SEARCHED_BEERS } from '../actions/index
 
 const beers = `https://api.punkapi.com/v2/beers`;
 const search = term => `${beers}?beer_name=${encodeURIComponent(term)}`;
-const ajax$ = term => ajax.getJSON(search(term));
+const ajax$ = term =>
+  term === 'skull'
+    ? throwError(new Error('failed!'))
+    : ajax.getJSON(search(term))
 
 function searchBeersEpic(action$) {
   return action$.pipe(
