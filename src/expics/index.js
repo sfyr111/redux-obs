5828fbf258b5106e008b7ddb5ca97e879c3878b2
sfyr111 <sfyr111@163.com>
2018-11-03 22:07:08 +0800

    l7

--- src/expics/index.js
+++ src/expics/index.js
@@ -1,24 +1,23 @@
 import { forkJoin } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
-import { switchMap, map, mergeMap } from 'rxjs/operators'
+import { switchMap, map, mergeMap, debounceTime } from 'rxjs/operators'
 import { combineEpics, ofType } from 'redux-observable'
-import { FETCH_STORIES, fetchStoriesFulfilledAction } from "../actions/index";
+import { receiveBeers, SEARCHED_BEERS } from "../actions/index";
 
-const topStories = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`;
-const url = id => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
+const beers = `https://api.punkapi.com/v2/beers`;
+const search = term => `${beers}?beer_name=${encodeURIComponent(term)}`;
+const ajax$ = term => ajax.getJSON(search(term));
 
-function loadStoriesEpic(action$) {
+function searchBeersEpic(action$) {
   return action$.pipe(
-    ofType(FETCH_STORIES),
-    switchMap(({ payload }) => ajax.getJSON(topStories)),
-    map(ids => ids.slice(0, 5)),
-    map(ids => ids.map(url)),
-    map(urls => urls.map(url => ajax.getJSON(url))), // 转换 url -> ajax
-    mergeMap(reqs => forkJoin(reqs)), // 执行 ajax 请求
-    map(stories => fetchStoriesFulfilledAction(stories))
+    ofType(SEARCHED_BEERS),
+    debounceTime(500),
+    switchMap(({ payload }) => ajax$(payload).pipe(
+      map(receiveBeers)
+    ))
   )
 }
 
-export const rootEpic = combineEpics(loadStoriesEpic)
+export const rootEpic = combineEpics(searchBeersEpic)
 
 
